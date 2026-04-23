effa6a4526867cc9019ee95e69246ca1a4a00ddf
thejas-tech-hub <thejasbejja42@gmail.com>
2026-04-23 21:20:26 +0530

    Fix: move Firebase config to environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_CpGlyBSiko9VyBv4HywodwWKIQnMdjo",
-  authDomain: "sevasphere-2026.firebaseapp.com",
-  projectId: "sevasphere-2026",
-  storageBucket: "sevasphere-2026.firebasestorage.app",
-  messagingSenderId: "521492905355",
-  appId: "1:521492905355:web:3bfc17f3e0d8413c85838e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
